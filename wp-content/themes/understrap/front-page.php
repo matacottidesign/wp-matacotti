e4e25ecb2ad583c19bca713ec2d411bf5df39d86
Francesco Matacotti <francesco1984.go@gmail.com>
2020-03-02 13:09:14 +0100

    sistemato parametri iniziali/pagine/widget

--- wp-content/themes/understrap/front-page.php
+++ wp-content/themes/understrap/front-page.php
@@ -31,16 +31,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 			<main class="site-main" id="main">
 
 				<?php while ( have_posts() ) : the_post(); ?>
-
-					<?php get_template_part( 'loop-templates/content', 'page' ); ?>
-
-					<?php
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-					?>
-
+                    <?php the_content(); ?>
 				<?php endwhile; // end of the loop. ?>
 
 			</main><!-- #main -->
