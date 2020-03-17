1bb7ceee4caa5f2b1f94bfe2cad39b67d28340b3
Francesco Matacotti <francesco1984.go@gmail.com>
2020-03-17 17:13:26 +0100

    completato pagina GRAFICA

--- wp-content/themes/understrap/page-templates/grafica.php
+++ wp-content/themes/understrap/page-templates/grafica.php
@@ -22,13 +22,13 @@ while ( have_posts() ) :
         <div class="row">
         <div class="col-12 col-lg-6 my-5">
             <h1 class="blue-primary mb-5">
-                <?php the_field('titolo1_web'); ?>
-                <span class="ml-3">&#128421;</span>
+                <?php the_field('titolo1_grafica'); ?>
+                <span class="ml-3">&#128396;</span>
             </h1>
-            <p><?php the_field('descrizione1_web'); ?></p>
+            <p><?php the_field('descrizione1_grafica'); ?></p>
             <button type="button" class="main-btn mt-5 btn btn-warning">
                 <?php 
-				$link = get_field('link1_web');
+				$link = get_field('link1_grafica');
 				if( $link ): 
 					$link_url = $link['url'];
 					$link_title = $link['title'];
@@ -40,7 +40,7 @@ while ( have_posts() ) :
         </div>
         <div class="col-12 col-lg-6 d-flex justify-content-center align-items-center my-5">
             <?php 
-			$image = get_field('immagine1_web');
+			$image = get_field('immagine1_grafica');
 			if( !empty( $image ) ): ?>
 				<img data-aos-duration="2000" data-aos="fade-right" class="w-100" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
 			<?php endif; ?>
@@ -49,6 +49,43 @@ while ( have_posts() ) :
         </div>
     </div>
 
+    <div class="container">
+    <!-- Modulo galleria -->
+    <?php if( have_rows('riga') ): ?>
+        <?php while( have_rows('riga') ): the_row(); ?>
+            
+        <h3 class="mt-5 mb-3"><?php the_sub_field('titolo_riga'); ?></h3>
+
+        <!-- Galleria immagini -->
+        <div class="test d-flex gallery-card mb-3">
+        <?php if( have_rows('modulo_riga') ): ?>
+            <?php while( have_rows('modulo_riga') ): the_row(); ?>
+            
+            <?php 
+            $image = get_sub_field('immagine_modulo');
+            if( !empty( $image ) ): ?>
+                <img class="w-100" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+            <?php endif; ?>
+
+            <?php endwhile; ?>
+        <?php endif; ?>
+        </div>
+
+        <div class="text-right">
+        <?php 
+        $link = get_sub_field('link_riga');
+        if( $link ): 
+            $link_url = $link['url'];
+            $link_title = $link['title'];
+            $link_target = $link['target'] ? $link['target'] : '_self';
+            ?>
+            <a class="button" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
+        <?php endif; ?>
+        </div>
+
+        <?php endwhile; ?>
+    <?php endif; ?>
+    </div>
 
   <!-- Cards -->
   <div class="bg-skills">
